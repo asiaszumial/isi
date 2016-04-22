c066db0bae4770597ee966ead45fddc51a9dff52
asia <szumial.joanna@gmail.com>
2016-04-22 14:49:18 +0200

    initial commit mvc template

---
+++ ISIProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ISIProject.Startup))]
+namespace ISIProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
