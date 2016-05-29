c5d36befa877ce419808999b06e076f832fb73b7
lukewaw <lukas_wawrzyniak@wp.pl>
2016-05-29 11:59:19 +0200

    Address changes.

--- ISIProject/Controllers/OrderController.cs
+++ ISIProject/Controllers/OrderController.cs
@@ -113,7 +113,7 @@ namespace ISIProject.Controllers
             OperationResult operationResult = new OperationResult();
             operationResult = transferData(operationResult, payments);
             //string address = ConfigurationManager.AppSettings["esbAddressToSendEmailNotification"].ToString();
-            string URLString = "http://0fc04ba5.ngrok.io/sendEmail";
+            string URLString = "http://c3561f8d.ngrok.io/sendEmail";
             try
             {
                 var httpWebRequest = (HttpWebRequest)WebRequest.Create(URLString);
